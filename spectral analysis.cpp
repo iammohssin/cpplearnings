2cfd2cfb3921323688192a06e27870762ea19e6f
Mohsin Javaid <iammohssin@users.noreply.github.com>
2020-08-08 21:47:19 +0500

    Create spectral analysis.cpp

---
+++ spectral analysis.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double wavelength;
+    cout <<"Enter wave length in meters:";
+    cin>>wavelength;
+
+    if (wavelength >= 1E-2)
+        cout << "Radio Waves" << endl;
+    else if (wavelength <= 1E-2 && wavelength >= 1E-3)
+        cout << "Microwaves" << endl;
+    else if (wavelength <= 1E-3 && wavelength >= 7E-7)
+        cout << "Infrared" << endl;
+    else if (wavelength <= 7E-7 && wavelength >= 4E-7)
+        cout << "Visible Light" << endl;
+    else if (wavelength <= 4E-7 && wavelength >= 1E-8)
+        cout << "Ultraviolet" << endl;
+    else if (wavelength <= 1E-8 && wavelength >= 1E-11)
+        cout << "X Rays" << endl;
+    else if (wavelength <= 1E-11)
+        cout << "Gamma Rays" << endl;
+    return 0;
+}
